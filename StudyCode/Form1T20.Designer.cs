ce9d830bdb9e2a190a79c4d0a362ab3841f856e6
JackChen <530216775@qq.com>
2018-05-31 23:38:23 +0800

    T20D1

---
+++ StudyCode/Form1T20.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace StudyCode
+{
+    partial class Form1T20
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbUrl = new System.Windows.Forms.TextBox();
+            this.btnDownLoad = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rtbState = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "下载地址：";
+            // 
+            // txbUrl
+            // 
+            this.txbUrl.Location = new System.Drawing.Point(85, 13);
+            this.txbUrl.Name = "txbUrl";
+            this.txbUrl.Size = new System.Drawing.Size(314, 21);
+            this.txbUrl.TabIndex = 1;
+            // 
+            // btnDownLoad
+            // 
+            this.btnDownLoad.Location = new System.Drawing.Point(422, 10);
+            this.btnDownLoad.Name = "btnDownLoad";
+            this.btnDownLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnDownLoad.TabIndex = 2;
+            this.btnDownLoad.Text = "下载";
+            this.btnDownLoad.UseVisualStyleBackColor = true;
+            this.btnDownLoad.Click += new System.EventHandler(this.btnDownLoad_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rtbState);
+            this.groupBox1.Location = new System.Drawing.Point(15, 40);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(499, 149);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "状态：";
+            // 
+            // rtbState
+            // 
+            this.rtbState.Location = new System.Drawing.Point(6, 20);
+            this.rtbState.Multiline = true;
+            this.rtbState.Name = "rtbState";
+            this.rtbState.Size = new System.Drawing.Size(487, 123);
+            this.rtbState.TabIndex = 0;
+            // 
+            // Form1T20
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 201);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnDownLoad);
+            this.Controls.Add(this.txbUrl);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1T20";
+            this.Text = "Form1T20";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txbUrl;
+        private System.Windows.Forms.Button btnDownLoad;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox rtbState;
+    }
+}
