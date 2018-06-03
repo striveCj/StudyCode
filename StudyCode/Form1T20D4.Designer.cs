e3659748ad12f426ce57fd7a0194fcb4cc2c6b43
JackChen <530216775@qq.com>
2018-06-03 23:57:04 +0800

    T20D4

---
+++ StudyCode/Form1T20D4.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace StudyCode
+{
+    partial class Form1T20D4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rtbState = new System.Windows.Forms.TextBox();
+            this.btnDownLoad = new System.Windows.Forms.Button();
+            this.txbUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btmPause = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rtbState);
+            this.groupBox1.Location = new System.Drawing.Point(12, 101);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(499, 149);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "状态：";
+            // 
+            // rtbState
+            // 
+            this.rtbState.Location = new System.Drawing.Point(6, 20);
+            this.rtbState.Multiline = true;
+            this.rtbState.Name = "rtbState";
+            this.rtbState.Size = new System.Drawing.Size(487, 123);
+            this.rtbState.TabIndex = 0;
+            // 
+            // btnDownLoad
+            // 
+            this.btnDownLoad.Location = new System.Drawing.Point(330, 41);
+            this.btnDownLoad.Name = "btnDownLoad";
+            this.btnDownLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnDownLoad.TabIndex = 14;
+            this.btnDownLoad.Text = "下载";
+            this.btnDownLoad.UseVisualStyleBackColor = true;
+            this.btnDownLoad.Click += new System.EventHandler(this.btnDownLoad_Click);
+            // 
+            // txbUrl
+            // 
+            this.txbUrl.Location = new System.Drawing.Point(82, 14);
+            this.txbUrl.Name = "txbUrl";
+            this.txbUrl.Size = new System.Drawing.Size(429, 21);
+            this.txbUrl.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "下载地址：";
+            // 
+            // btmPause
+            // 
+            this.btmPause.Location = new System.Drawing.Point(430, 41);
+            this.btmPause.Name = "btmPause";
+            this.btmPause.Size = new System.Drawing.Size(75, 23);
+            this.btmPause.TabIndex = 16;
+            this.btmPause.Text = "暂停";
+            this.btmPause.UseVisualStyleBackColor = true;
+            this.btmPause.Click += new System.EventHandler(this.btmPause_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(18, 70);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(487, 23);
+            this.progressBar1.TabIndex = 17;
+            // 
+            // Form1T20D4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 262);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btmPause);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnDownLoad);
+            this.Controls.Add(this.txbUrl);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1T20D4";
+            this.Text = "Form1T20D4";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox rtbState;
+        private System.Windows.Forms.Button btnDownLoad;
+        private System.Windows.Forms.TextBox txbUrl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btmPause;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
---
+++ StudyCode/Form1T20D4.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Remoting.Messaging;
+using System.Threading;
+using System.Net;
+
+namespace StudyCode
+{
+    public partial class Form1T20D4 : Form
+    {
+        public int DownloadSize = 0;
+        public string downloadPath = null;
+        long totalSize = 0;
+        const int BufferSize = 2048;
+        byte[] BufferRead = new byte[BufferSize];
+        FileStream filestream = null;
+        HttpWebResponse myWebResponse = null;
+        public Form1T20D4()
+        {
+            InitializeComponent();
+            string url = "http://download.microsoft.com/download/7/0/3/70345ee-a747-4cc8-bd3e-98a615c3aedb/dotNetFx35setup.exe";
+            txbUrl.Text = url;
+            this.btmPause.Enabled = false;
+            GetTotalSize();
+            downloadPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\" + Path.GetFileName(this.txbUrl.Text.Trim());
+            if (File.Exists(downloadPath))
+            {
+                FileInfo fileInfo = new FileInfo(downloadPath);
+                DownloadSize = (int)fileInfo.Length;
+                progressBar1.Value = (int)((float)DownloadSize / (float)totalSize * 100);
+            }
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+        }
+
+        private void btnDownLoad_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.IsBusy!=true)
+            {
+                backgroundWorker1.RunWorkerAsync();
+                filestream = new FileStream(downloadPath, FileMode.OpenOrCreate);
+                filestream.Seek(DownloadSize, SeekOrigin.Begin);
+                this.btnDownLoad.Enabled = false;
+                this.btmPause.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("正在执行操作，请稍后");
+            }
+        }
+
+        private void btmPause_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.IsBusy&&backgroundWorker1.WorkerSupportsCancellation==true)
+            {
+                backgroundWorker1.CancelAsync();
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
+
+        private void GetTotalSize()
+        {
+            HttpWebRequest myHttpWebRequest = (HttpWebRequest)WebRequest.Create(txbUrl.Text.Trim());
+            HttpWebResponse response = (HttpWebResponse)myHttpWebRequest.GetResponse();
+            totalSize = response.ContentLength;
+            response.Close();
+        }
+    }
+}
