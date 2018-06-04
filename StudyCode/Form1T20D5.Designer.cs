ce635ffe1b2bf70e6c5671067a60ca345c886be8
JackChen <530216775@qq.com>
2018-06-04 23:54:18 +0800

    T20D5

---
+++ StudyCode/Form1T20D5.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace StudyCode
+{
+    partial class Form1T20D5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btmPause = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rtbState = new System.Windows.Forms.TextBox();
+            this.btnDownLoad = new System.Windows.Forms.Button();
+            this.txbUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(20, 69);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(487, 23);
+            this.progressBar1.TabIndex = 23;
+            // 
+            // btmPause
+            // 
+            this.btmPause.Location = new System.Drawing.Point(432, 40);
+            this.btmPause.Name = "btmPause";
+            this.btmPause.Size = new System.Drawing.Size(75, 23);
+            this.btmPause.TabIndex = 22;
+            this.btmPause.Text = "暂停";
+            this.btmPause.UseVisualStyleBackColor = true;
+            this.btmPause.Click += new System.EventHandler(this.btmPause_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rtbState);
+            this.groupBox1.Location = new System.Drawing.Point(14, 100);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(499, 149);
+            this.groupBox1.TabIndex = 21;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "状态：";
+            // 
+            // rtbState
+            // 
+            this.rtbState.Location = new System.Drawing.Point(6, 20);
+            this.rtbState.Multiline = true;
+            this.rtbState.Name = "rtbState";
+            this.rtbState.Size = new System.Drawing.Size(487, 123);
+            this.rtbState.TabIndex = 0;
+            // 
+            // btnDownLoad
+            // 
+            this.btnDownLoad.Location = new System.Drawing.Point(332, 40);
+            this.btnDownLoad.Name = "btnDownLoad";
+            this.btnDownLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnDownLoad.TabIndex = 20;
+            this.btnDownLoad.Text = "下载";
+            this.btnDownLoad.UseVisualStyleBackColor = true;
+            this.btnDownLoad.Click += new System.EventHandler(this.btnDownLoad_Click);
+            // 
+            // txbUrl
+            // 
+            this.txbUrl.Location = new System.Drawing.Point(84, 13);
+            this.txbUrl.Name = "txbUrl";
+            this.txbUrl.Size = new System.Drawing.Size(429, 21);
+            this.txbUrl.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "下载地址：";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // Form1T20D5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 262);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btmPause);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnDownLoad);
+            this.Controls.Add(this.txbUrl);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1T20D5";
+            this.Text = "Form1T20D5";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btmPause;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox rtbState;
+        private System.Windows.Forms.Button btnDownLoad;
+        private System.Windows.Forms.TextBox txbUrl;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
---
+++ StudyCode/Form1T20D5.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Remoting.Messaging;
+using System.Threading;
+using System.Net;
+
+namespace StudyCode
+{
+    public partial class Form1T20D5 : Form
+    {
+        int DownloadSize = 0;
+        string downloadPath = null;
+        long totalSize = 0;
+        FileStream filestream;
+        CancellationTokenSource cts = null;
+        Task task = null;
+        SynchronizationContext sc;
+        public Form1T20D5()
+        {
+            InitializeComponent();
+            string url = "http://download.microsoft.com/download/7/0/3/70345ee-a747-4cc8-bd3e-98a615c3aedb/dotNetFx35setup.exe";
+            txbUrl.Text = url;
+            this.btmPause.Enabled = false;
+            GetTotalSize();
+            downloadPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\" + Path.GetFileName(this.txbUrl.Text.Trim());
+            if (File.Exists(downloadPath))
+            {
+                FileInfo fileInfo = new FileInfo(downloadPath);
+                DownloadSize = (int)fileInfo.Length;
+                progressBar1.Value = (int)((float)DownloadSize / (float)totalSize * 100);
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
+
+        private void btnDownLoad_Click(object sender, EventArgs e)
+        {
+            filestream = new FileStream(downloadPath, FileMode.OpenOrCreate);
+            this.btnDownLoad.Enabled = false;
+            this.btmPause.Enabled = true;
+            filestream.Seek(DownloadSize, SeekOrigin.Begin);
+            sc = SynchronizationContext.Current;
+            cts = new CancellationTokenSource();
+            task = new Task(() => DownloadFileWithTAP(txbUrl.Text.Trim(), cts.Token, new Progress<int>(p =>
+            {
+                sc.Post(new SendOrPostCallback((result) => progressBar1.Value = (int)result), p);
+            })));
+            task.Start();
+        }
+        public void DownloadFileWithTAP(string url,CancellationToken ct,IProgress<int> progress)
+        {
+            HttpWebRequest request = null;
+            HttpWebResponse response = null;
+            Stream responseStream = null;
+            int bufferSize = 2048;
+            byte[] bufferBytes = new byte[bufferSize];
+            try
+            {
+                request = (HttpWebRequest)WebRequest.Create(url);
+                if (DownloadSize!=0)
+                {
+                    request.AddRange(DownloadSize);
+                }
+                response = (HttpWebResponse)request.GetResponse();
+                responseStream = response.GetResponseStream();
+                int readSize = 0;
+                while (true)
+                {
+                    if (ct.IsCancellationRequested==true)
+                    {
+                        MessageBox.Show($"下载暂停，下载的文件地址为：{downloadPath}，\n已下载的字节数为：{DownloadSize}");
+                        response.Close();
+                        filestream.Close();
+                        sc.Post((state) =>
+                        {
+                            this.btnDownLoad.Enabled = true;
+                            this.btmPause.Enabled = false;
+                        }, null);
+                        break;
+                    }
+                    readSize = responseStream.Read(bufferBytes, 0, bufferBytes.Length);
+                    if (readSize>0)
+                    {
+                        DownloadSize += readSize;
+                        int percentComplete= (int)((float)DownloadSize / (float)totalSize * 100);
+                        filestream.Write(bufferBytes, 0, readSize);
+                        progress.Report(percentComplete);
+                    }
+                    else
+                    {
+                        MessageBox.Show($"下载已完成，下载的文件地址为：{downloadPath}，\n字节总数为：{DownloadSize}");
+                        sc.Post((state) =>
+                        {
+                            this.btnDownLoad.Enabled = false;
+                            this.btmPause.Enabled = false;
+                        }, null);
+                        response.Close();
+                        filestream.Close();
+                        break;
+                    }
+                }
+            }
+            catch (AggregateException ex)
+            {
+                ex.Handle(e => e is OperationCanceledException);
+                throw;
+            }
+        }
+        private void btmPause_Click(object sender, EventArgs e)
+        {
+            cts.Cancel();
+        }
+        private void GetTotalSize()
+        {
+            HttpWebRequest myHttpWebRequest = (HttpWebRequest)WebRequest.Create(txbUrl.Text.Trim());
+            HttpWebResponse response = (HttpWebResponse)myHttpWebRequest.GetResponse();
+            totalSize = response.ContentLength;
+            response.Close();
+        }
+    }
+}
