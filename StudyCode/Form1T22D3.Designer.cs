aa88a68951d376300d320f7cab89082309d60681
JackChen <530216775@qq.com>
2018-06-10 23:38:36 +0800

    T22D3

---
+++ StudyCode/Form1T22D3.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace StudyCode
+{
+    partial class Form1T22D3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.tbxLocalIp = new System.Windows.Forms.TextBox();
+            this.tbxLocalPort = new System.Windows.Forms.TextBox();
+            this.tbxSendToIp = new System.Windows.Forms.TextBox();
+            this.tbxSendToPort = new System.Windows.Forms.TextBox();
+            this.tbxMessageSend = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnReceive = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "本地IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "发送到";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(201, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(201, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "内容：";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(28, 181);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(379, 148);
+            this.listBox1.TabIndex = 5;
+            // 
+            // tbxLocalIp
+            // 
+            this.tbxLocalIp.Location = new System.Drawing.Point(86, 10);
+            this.tbxLocalIp.Name = "tbxLocalIp";
+            this.tbxLocalIp.Size = new System.Drawing.Size(100, 21);
+            this.tbxLocalIp.TabIndex = 6;
+            // 
+            // tbxLocalPort
+            // 
+            this.tbxLocalPort.Location = new System.Drawing.Point(224, 10);
+            this.tbxLocalPort.Name = "tbxLocalPort";
+            this.tbxLocalPort.Size = new System.Drawing.Size(100, 21);
+            this.tbxLocalPort.TabIndex = 7;
+            // 
+            // tbxSendToIp
+            // 
+            this.tbxSendToIp.Location = new System.Drawing.Point(86, 37);
+            this.tbxSendToIp.Name = "tbxSendToIp";
+            this.tbxSendToIp.Size = new System.Drawing.Size(100, 21);
+            this.tbxSendToIp.TabIndex = 8;
+            // 
+            // tbxSendToPort
+            // 
+            this.tbxSendToPort.Location = new System.Drawing.Point(224, 41);
+            this.tbxSendToPort.Name = "tbxSendToPort";
+            this.tbxSendToPort.Size = new System.Drawing.Size(100, 21);
+            this.tbxSendToPort.TabIndex = 9;
+            // 
+            // tbxMessageSend
+            // 
+            this.tbxMessageSend.Location = new System.Drawing.Point(86, 99);
+            this.tbxMessageSend.Name = "tbxMessageSend";
+            this.tbxMessageSend.Size = new System.Drawing.Size(194, 21);
+            this.tbxMessageSend.TabIndex = 10;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(286, 96);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 11;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(28, 141);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "清空";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(132, 141);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 13;
+            this.btnStop.Text = "停止";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnReceive
+            // 
+            this.btnReceive.Location = new System.Drawing.Point(286, 141);
+            this.btnReceive.Name = "btnReceive";
+            this.btnReceive.Size = new System.Drawing.Size(75, 23);
+            this.btnReceive.TabIndex = 14;
+            this.btnReceive.Text = "接收";
+            this.btnReceive.UseVisualStyleBackColor = true;
+            this.btnReceive.Click += new System.EventHandler(this.btnReceive_Click);
+            // 
+            // Form1T22D3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 341);
+            this.Controls.Add(this.btnReceive);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.tbxMessageSend);
+            this.Controls.Add(this.tbxSendToPort);
+            this.Controls.Add(this.tbxSendToIp);
+            this.Controls.Add(this.tbxLocalPort);
+            this.Controls.Add(this.tbxLocalIp);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1T22D3";
+            this.Text = "Form1T22D3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox tbxLocalIp;
+        private System.Windows.Forms.TextBox tbxLocalPort;
+        private System.Windows.Forms.TextBox tbxSendToIp;
+        private System.Windows.Forms.TextBox tbxSendToPort;
+        private System.Windows.Forms.TextBox tbxMessageSend;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnReceive;
+    }
+}
---
+++ StudyCode/Form1T22D3.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudyCode
+{
+    public partial class Form1T22D3 : Form
+    {
+        private UdpClient sendUdpClient;
+        private UdpClient receiveUpdClient;
+        public Form1T22D3()
+        {
+            InitializeComponent();
+            IPAddress[] ips = Dns.GetHostAddresses("");
+            foreach (var ip in ips)
+            {
+                tbxLocalIp.Text = ip.ToString();
+                tbxSendToIp.Text = ip.ToString();
+                break;
+            }
+            int port = 51883;
+            int sendPort = 11883;
+            tbxSendToPort.Text = sendPort.ToString();
+            tbxLocalPort.Text = port.ToString(); 
+        }
+
+        private void ReceiveMessage()
+        {
+            if (receiveUpdClient==null)
+            {
+                return;
+            }
+            IPEndPoint remoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            while (true)
+            {
+                try
+                {
+                    byte[] receiveBytes = receiveUpdClient.Receive(ref remoteIpEndPoint);
+                    string message = Encoding.Unicode.GetString(receiveBytes);
+                    
+                }
+                catch
+                {
+
+                    break;
+                }
+            }
+        }
+        delegate void ShowMessageforViewCallBack(ListBox listbox, string text);
+
+        private void ShowMessageforView(ListBox listbox, string text)
+        {
+            if (listbox.InvokeRequired)
+            {
+                ShowMessageforViewCallBack showMessageforViewCallback = ShowMessageforView;
+                listbox.Invoke(showMessageforViewCallback, new object[] { listbox, text });
+
+            }
+            else
+            {
+                listBox1.Items.Add(text);
+                listBox1.SelectedIndex = listBox1.Items.Count - 1;
+                listBox1.ClearSelected();
+            }
+        }
+
+        private void SendMessage(object obj)
+        {
+            string message=(string)obj;
+            byte[] sendbytes = Encoding.Unicode.GetBytes(message);
+            IPAddress remoteIp = IPAddress.Parse(tbxSendToIp.Text);
+            IPEndPoint remoteIpEndPoint = new IPEndPoint(remoteIp, int.Parse(tbxSendToPort.Text);
+            sendUdpClient.Send(sendbytes, sendbytes.Length, remoteIpEndPoint);
+            sendUdpClient.Close();
+            ResetMessageText(tbxMessageSend);
+
+        }
+
+        delegate void ResetMessageCallback(TextBox textbox);
+        private void ResetMessageText(TextBox textbox)
+        {
+            if (textbox.InvokeRequired)
+            {
+                ResetMessageCallback resetMessagecallback = ResetMessageText;
+                textbox.Clear();
+                textbox.Focus();
+            }
+        }
+
+        private void btnReceive_Click(object sender, EventArgs e)
+        {
+            IPAddress localIp = IPAddress.Parse(tbxLocalIp.Text);
+            IPEndPoint localIpEndPoint = new IPEndPoint(localIp, int.Parse(tbxLocalPort.Text));
+            try
+            {
+                receiveUpdClient = new UdpClient(localIpEndPoint);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (tbxMessageSend.Text==string.Empty)
+            {
+                MessageBox.Show("发送内容不能为空", "提示");
+                return;
+            }
+            IPAddress localIp = IPAddress.Parse(tbxLocalIp.Text);
+            IPEndPoint localIpEndPoint = new IPEndPoint(localIp, int.Parse(tbxLocalPort.Text));
+            sendUdpClient = new UdpClient(localIpEndPoint);
+
+            Thread sendThread = new Thread(SendMessage);
+            sendThread.Start(tbxMessageSend.Text);
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            receiveUpdClient.Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            this.listBox1.Items.Clear();
+        }
+    }
+}
