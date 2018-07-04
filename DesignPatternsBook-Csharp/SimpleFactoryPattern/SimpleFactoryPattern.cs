b32b391afe3483d9b75f42a092c488f9fa0a0777
JackChen <530216775@qq.com>
2018-07-04 23:57:00 +0800

    大化设计模式T1

---
+++ DesignPatternsBook-Csharp/SimpleFactoryPattern/SimpleFactoryPattern.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsBook_Csharp
+{
+    /// <summary>
+    /// 简单工厂模式
+    /// </summary>
+    public class SimpleFactoryPattern
+    {
+
+    }
+}
