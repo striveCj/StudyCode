01597bd94e7afaa46083e558de053ba8f6202ebb
JackChen <530216775@qq.com>
2018-07-07 16:05:15 +0800

    策略模式

---
+++ DesignPatternsBook-Csharp/StrategyPattern/ConcreteStrategyC.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsBook_Csharp.StrategyPattern
+{
+    class ConcreteStrategyC:Strategy
+    {
+        public override void AlgorithmInterface()
+        {
+            Console.WriteLine("算法C实现");
+        }
+    }
+}
---
+++ DesignPatternsBook-Csharp/StrategyPattern/Strategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsBook_Csharp.StrategyPattern
+{
+    abstract class Strategy
+    {
+        /// <summary>
+        /// 算法方法
+        /// </summary>
+        public abstract void AlgorithmInterface();
+    }
+}
