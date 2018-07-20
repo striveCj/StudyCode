b91082d4c645558f66b489ab85961627b47e7004
JackChen <530216775@qq.com>
2018-07-20 23:47:24 +0800

    备忘录模式

---
+++ DesignPatternsBook-Csharp/MementoPattern/Caretaker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsBook_Csharp.MementoPattern
+{
+    class Caretaker
+    {
+        private string state;
+        public string State { get { return state; } set { state = value; } }
+    }
+}
