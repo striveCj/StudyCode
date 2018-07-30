82611b8deafab2aaed3d02f687a016fa121ab38f
JackChen <530216775@qq.com>
2018-07-30 23:51:00 +0800

    访问者模式

---
+++ DesignPatternsBook-Csharp/VisitorPattern/Visitor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsBook_Csharp.VisitorPattern
+{
+    abstract class Visitor
+    {
+        public abstract void VisitConcreteElementA(ConcreteElementA concreteElementA);
+        public abstract void VisitConcreteElementB(ConcreteElementB concreteElementB);
+    }
+}
