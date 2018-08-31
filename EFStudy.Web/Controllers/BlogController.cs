461e90c82413c957cc4f45b872eba85f5a9f459c
JackChen <530216775@qq.com>
2018-09-01 00:53:10 +0800

    删除单个博客

--- EFStudy.Web/Controllers/BlogController.cs
+++ EFStudy.Web/Controllers/BlogController.cs
@@ -55,12 +55,55 @@ namespace EFStudy.Web.Controllers
         {
             return JsonConvert.DeserializeObject<Person>(p);
         }
-
+        /// <summary>
+        /// 获取单个博客实体 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public async Task<ActionResult> Get(int? id)
         {
             if (ReferenceEquals(id,null)||id.Value<=0)
             {
-
+                return Content("<script type='text/javascript'>alert('提交参数不正确!');location.href='/'</script>");
+            }
+            using (var _context=new EFDbContext())
+            {
+                var blog = await _context.Blogs.FindAsync(id);
+                if (ReferenceEquals(blog,null))
+                {
+                    return Content("<script type='text/javascript'>alert('改博客不存在!');location.href='/'</script>");
+                }
+                return View("UpInsert", blog);
+            }
+        }
+        /// <summary>
+        /// 删除单个博客
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (ReferenceEquals(id, null) || id.Value <= 0)
+            {
+                return Content("<script type='text/javascript'>alert('提交参数不正确!');location.href='/'</script>");
+            }
+            using (var _context = new EFDbContext())
+            {
+                var dbBlog = await _context.Blogs.FindAsync(id);
+                if (ReferenceEquals(dbBlog, null))
+                {
+                    return Content("<script type='text/javascript'>alert('该博客不存在!');location.href='/'</script>");
+                }
+                else
+                {
+                    _context.Blogs.Remove(dbBlog);
+                    var result = _context.SaveChanges();
+                    if (result>0)
+                    {
+                        return View("Index");
+                    }
+                    return Content("<script type='text/javascript'>alert('删除失败!');location.href='/'</script>");
+                }
             }
         }
     }
