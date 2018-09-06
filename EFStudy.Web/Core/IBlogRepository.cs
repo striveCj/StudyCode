4464eec4775b420aac912dacdb22c8719588b6f1
JackChen <530216775@qq.com>
2018-09-06 23:59:58 +0800

    仓储搭建

---
+++ EFStudy.Web/Core/IBlogRepository.cs
@@ -0,0 +1,14 @@
+﻿using EFStudy.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFStudy.Web.Core
+{
+    public interface IBlogRepository
+    {
+        Blog Get(EFDbContext context, Guid BlogId);
+    }
+}
