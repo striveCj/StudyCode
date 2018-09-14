c9620e0bb270f41ba12fdfd7e01e8229de131053
iZg1s1p6oyu92wZ\Administrator <530216775@qq.com>
2018-09-14 13:29:32 +0800

    monitor类锁定资源ipad测试提交

--- CShapMultithreading/T1/T1.cs
+++ CShapMultithreading/T1/T1.cs
@@ -266,6 +266,21 @@ namespace CShapMultithreading.T1
             object lock1 = new object();
             object lock2 = new object();
             new Thread(() => LockTooMuch(lock1, lock2));
+            lock (lock2)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Monitor.ThyEnter allows not to get stuck ,returning false after a specified timeout is elapsed ");
+                if (Monitor.TryEnter(lock1,TimeSpan.FromSeconds(5)))
+                {
+                    Console.WriteLine("Acquired a protected resource succesfully");
+                }
+                else
+                {
+                    Console.WriteLine( "Timeout acquiring a resource!");
+                }
+            }
+            new Thread(() => LockTooMuch(lock1, lock2)).Start();
+            Console.WriteLine("-—-—-—-—-—-—-—-—");
         }
         static void LockTooMuch(object lock1,object lock2)
         {
