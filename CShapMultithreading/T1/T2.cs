e6043b96156080c2947c305d75de73c320cb59d5
iZg1s1p6oyu92wZ\Administrator <530216775@qq.com>
2018-09-23 01:03:17 +0800

    使用Mutex类

--- CShapMultithreading/T1/T2.cs
+++ CShapMultithreading/T1/T2.cs
@@ -50,7 +50,18 @@ namespace CShapMultithreading.T1
         }
         public void test()
         {
+            const string MutexName = "CSharpThreadingCookbook";
+            using (var m=new Mutex(false,MutexName))
+            {
+                if (!m.WaitOne(TimeSpan.FromSeconds(5),false))
+                {
+                    Console.WriteLine("Second instance is running");
+                }
+                else { Console.WriteLine("Running");
 
+                    Console.ReadLine();
+                    m.ReleaseMutex();
+            }
         }
     }
 
