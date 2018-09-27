c65ab1bba9872e0d5684333a874b8e87e00ab62d
JackChen <530216775@qq.com>
2018-09-27 23:49:50 +0800

    调用DetectChange方法的时机

--- EFStudy/Model/T5/T5Function.cs
+++ EFStudy/Model/T5/T5Function.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,31 @@ namespace EFStudy.Model.T5
                 var post = context.Posts.Single(p => p.Title == "My First Post");
                 post.Title = "My Best Post";
                 context.SaveChanges();
+            }
+        }
+
+        public static void T5D2()
+        {
+            using (var context=new AnotherBlogContext())
+            {
+                context.Blogs.Load();
+                var post = context.Posts.Single(p => p.Title == "My First Post");
+                post.Title = "My Best Post";
+                post.BlogId = 7;
+                context.SaveChanges();
+            }
+        }
+
+        public static void T5D3()
+        {
+            using (var context = new AnotherBlogContext())
+            {
+                context.Blogs.Load();
+                var post = context.Posts.Single(p => p.Title == "My First Post");
+                Console.WriteLine(context.Entry(post).State);
+                //或者
+                var entry = context.Entry<Post>(post);
+                Console.WriteLine(entry.State);
             }
         }
     }
