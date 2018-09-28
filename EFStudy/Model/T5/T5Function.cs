80e00a5610e8090c99339ea6812c607cd40e9599
JackChen <530216775@qq.com>
2018-09-28 21:45:20 +0800

    日志管理

--- EFStudy/Model/T5/T5Function.cs
+++ EFStudy/Model/T5/T5Function.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EFStudy.Core;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -52,5 +54,87 @@ namespace EFStudy.Model.T5
                 var blog2 = context.Blogs.Find(2);
             }
         }
+
+        public static void AddPosts(List<Post> Posts)
+        {
+            using (var context=new AnotherBlogContext())
+            {
+                try
+                {
+                    context.Configuration.AutoDetectChangesEnabled = false;
+                    Posts.ForEach(p => context.Posts.Add(p));
+                }
+                finally
+                {
+                    context.Configuration.AutoDetectChangesEnabled = true;
+                }
+                context.SaveChanges();
+            }
+        }
+
+        public static void AttachAndMovePosts(Blog efBlog,List<Post> Posts)
+        {
+            using (var context=new AnotherBlogContext())
+            {
+                try
+                {
+                    context.Configuration.AutoDetectChangesEnabled = false;
+                    context.Blogs.Attach(efBlog);
+                    Posts.ForEach(p =>
+                    {
+                        context.Posts.Attach(p);
+                        if (p.Title.StartsWith("Entity Framework:"))
+                        {
+                            context.Entry(p).Property(p2 => p2.Title).CurrentValue = p.Title.Replace("Entity Framework:", "EF:");
+                            context.Entry(p).Reference(p2 => p2.Blog).CurrentValue = efBlog;
+                        }
+                    });
+                    context.SaveChanges();
+                }
+                finally
+                {
+                    context.Configuration.AutoDetectChangesEnabled = true;
+                }
+            }
+        }
+        /// <summary>
+        /// 日志记录 toString 打印
+        /// </summary>
+        public static void T5D5()
+        {
+            using (var ctx=new EfDbContext())
+            {
+                var blog = ctx.Customer;
+                var sql = blog.ToString();
+                Console.WriteLine(sql);
+            }
+        }
+
+        public static void T5D6()
+        {
+            using (var ctx = new EfDbContext())
+            {
+                ctx.Database.Log = Console.WriteLine;
+                var cutsomer = ctx.Customer.FirstOrDefault(d => d.Id == 1);
+            }
+        }
+
+        public static void T5D7()
+        {
+            using (var ctx = new EfDbContext())
+            {
+                ctx.Database.Log = msg => MyLogger.Log("EFLoggingDemo", msg);
+                var cutsomer = ctx.Customer.FirstOrDefault(d => d.Id == 1);
+            }
+        }
+
+    }
+    public class MyLogger
+    {
+        public static void Log(string application,string message)
+        {
+            Console.WriteLine($"Application:{application},EF Message{message}");
+        }
+
     }
 }
