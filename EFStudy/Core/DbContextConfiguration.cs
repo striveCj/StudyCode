108734d53cad7c5784c7ac54267ca4e782d84967
JackChen <530216775@qq.com>
2018-10-01 23:46:45 +0800

    构造块拦截

---
+++ EFStudy/Core/DbContextConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure.Interception;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFStudy.Core
+{
+     public class DbContextConfiguration:DbConfiguration
+    {
+        public DbContextConfiguration()
+        {
+            DbInterception.Add(new NLogCommandInterceptor());
+        }
+    }
+}
