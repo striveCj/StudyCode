7e4832f2382882bc13ff8adb0585aa67014424aa
JackChen <530216775@qq.com>
2018-10-25 23:37:35 +0800

    实战6.X

---
+++ EF.Core/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.Core
+{
+    public abstract class BaseEntity
+    {
+        public Int64 ID { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime ModifiedTime { get; set; }
+    }
+}
---
+++ EF.Core/Model/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.Core.Model
+{
+    public class Book:BaseEntity
+    {
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string ISBN { get; set; }
+
+        public DateTime Published { get; set; }
+
+        public string IP { get; set; }
+
+        public string Url { get; set; }
+
+    }
+}
