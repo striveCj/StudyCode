6dc9ebe9f56f01edba8cdfdfc427ae5088e63f6c
JackChen <530216775@qq.com>
2018-10-27 15:29:15 +0800

    增加Polly重试机制

--- EF.Core/Helper/FileHelper.cs
+++ EF.Core/Helper/FileHelper.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace EF.Core.Model
+namespace EF.Core.Helper
 {
     public static class FileHelper
     {
@@ -52,6 +49,23 @@ namespace EF.Core.Model
         public static string GetFileNameWithoutExtension(string path)
         {
             return Path.GetFileNameWithoutExtension(path);
+        }
+
+        public static string GetMD5HashOfFile(string file)
+        {
+            var md5 = new MD5CryptoServiceProvider();
+            var stream=new FileStream(file,FileMode.Open,FileAccess.Read,FileShare.Read,8192);
+            md5.ComputeHash(stream);
+            stream.Close();
+            byte[] hash = md5.Hash;
+            var sb=new StringBuilder();
+            foreach (byte b in hash)
+            {
+                sb.Append(string.Format("{0:x2}", b));
+            }
+
+            return sb.ToString();
+
         }
 
 
