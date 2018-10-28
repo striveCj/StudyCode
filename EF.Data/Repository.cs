27a2d975325d2bed3ea47b1c16cafefa7369273c
JackChen <530216775@qq.com>
2018-10-28 22:54:22 +0800

    仓储实现，工作单元模式实现

--- EF.Data/Repository.cs
+++ EF.Data/Repository.cs
@@ -48,7 +48,73 @@ namespace EF.Data
                             Environment.NewLine;
                     }
                     throw new Exception(errorMessage,dbEx);
+                } 
+            }
+        }
+        public void Update(T entity)
+        {
+            try
+            {
+                if (entity == null)
+                {
+                    throw new ArgumentNullException(nameof(entity));
                 }
+            }
+            catch (DbEntityValidationException dbEx)
+            {
+                foreach (var validationErrors in dbEx.EntityValidationErrors)
+                {
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                    {
+                        errorMessage +=
+                            $"Property:{validationError.PropertyName} Error:{validationError.ErrorMessage}" +
+                            Environment.NewLine;
+                    }
+                    throw new Exception(errorMessage, dbEx);
+                }
+            }
+        }
+
+        public void Delete(T entity)
+        {
+            try
+            {
+                if (entity == null)
+                {
+                    throw new ArgumentNullException(nameof(entity));
+                }
+
+                this.entities.Remove(entity);
+            }
+            catch (DbEntityValidationException dbEx)
+            {
+                foreach (var validationErrors in dbEx.EntityValidationErrors)
+                {
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                    {
+                        errorMessage +=
+                            $"Property:{validationError.PropertyName} Error:{validationError.ErrorMessage}" +
+                            Environment.NewLine;
+                    }
+                    throw new Exception(errorMessage, dbEx);
+                }
+            }
+        }
+
+        public virtual IQueryable<T> Table
+        {
+            get { return this.entities; }
+        }
+
+        public IDbSet<T> Entities
+        {
+            get
+            {
+                if (entities==null)
+                {
+                    entities = context.Set<T>();
+                }
+                return entities;
             }
         }
     }
---
+++ EF.Data/UnitOfWork.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EF.Core;
+using EF.Core.Data;
+
+namespace EF.Data
+{
+    public class UnitOfWork:IUnitOfWork,IDisposable
+    {
+        private readonly EFDbContext context;
+        private bool disposed;
+        private ConcurrentDictionary<string, object> repositories;
+
+        public UnitOfWork(EFDbContext context)
+        {
+            this.context = context;
+        }
+
+        public UnitOfWork()
+        {
+            context=new EFDbContext();
+        }
+
+        public void Commit()
+        {
+            context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void RpllBack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            disposed = true;
+        }
+
+        public Repository<T> Repository<T>() where T : BaseEntity
+        {
+            if (repositories==null)
+            {
+                repositories=new ConcurrentDictionary<string, object>();
+            }
+            var type = typeof(T).Name;
+            if (!repositories.ContainsKey(type))
+            {
+                var repositoryType = typeof(Repository<>);
+                var repositoryInstance = Activator.CreateInstance(repositoryType.MakeGenericType(typeof(T)), context);
+                repositories.TryAdd(type, repositoryInstance);
+            }
+            return (Repository<T>) repositories[type];
+        }
+    }
+}
