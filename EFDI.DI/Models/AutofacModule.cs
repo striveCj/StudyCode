d7547df530b33ca2a1cb410942eb01ed8adeeacb
JackChen <530216775@qq.com>
2018-11-04 01:09:13 +0800

    aotufac依赖

---
+++ EFDI.DI/Models/AutofacModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Autofac;
+using EFDI.Data;
+using EFDI.Service;
+
+namespace EFDI.DI.Models
+{
+    public class AutofacModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<EFDbContext>().As<IDbContext>().InstancePerLifetimeScope();
+            builder.RegisterGeneric(typeof(Repository<>)).As(typeof(IRepository<>)).InstancePerLifetimeScope();
+            builder.RegisterType<UserService>().As<IUserService>();
+            base.Load(builder);
+        }
+    }
+}
