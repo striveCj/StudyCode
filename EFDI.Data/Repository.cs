07da96a15a3e0ba424bf7b025e9f2b9b9201b9b2
JackChen <530216775@qq.com>
2018-11-03 15:31:04 +0800

    仓储方法完成

--- EFDI.Data/Repository.cs
+++ EFDI.Data/Repository.cs
@@ -11,48 +11,129 @@ namespace EFDI.Data
 {
     public class Repository<T> : IRepository<T> where T : BaseEntity
     {
-        private readonly IDbContext _context;
+        private readonly DbContext _context;
         private IDbSet<T> _entities;
 
-        public Repository(IDbContext context)
+        public Repository(DbContext context)
         {
             _context = context;
         }
 
+        private IDbSet<T> Entities
+        {
+            get
+            {
+                if (_entities==null)
+                {
+                    _entities = _context.Set<T>();
+                }
+
+                return _entities;
+            }
+        }
+        public IQueryable<T> Table
+        {
+            get { return Entities; }
+        }
+
+        public void Delete(T entity)
+        {
+            try
+            {
+
+                if (entity == null)
+                {
+                    throw new ArgumentNullException(nameof(entity));
+                }
+
+                Entities.Remove(entity);
+                _context.SaveChanges();
+            }
+            catch (DbEntityValidationException e)
+            {
+                var msg = string.Empty;
+
+                foreach (var validationErrors in e.EntityValidationErrors)
+                {
+                    foreach (var validtionError in validationErrors.ValidationErrors)
+                    {
+                        //TODO:在 Windows 环境中，C# 语言 Environment.NewLine == "\r\n" 结果为 true。
+                        msg += string.Format(
+                            $"Property:{validtionError.PropertyName} Error{validtionError.ErrorMessage}" +
+                            Environment.NewLine);
+                    }
+                }
+                var fail = new Exception(msg, e);
+                throw fail;
+            }
+        }
+
         public T GetById(object id)
         {
-            return _entities.Find(id);
+            return Entities.Find(id);
         }
 
         public void Insert(T entity)
         {
             try
             {
-                if (entity==null)
-                {
+
                     if (entity==null)
                     {
                         throw new ArgumentNullException(nameof(entity));
                     }
 
-                }
-
-                _entities.Add(entity);
+                Entities.Add(entity);
                 _context.SaveChanges();
             }
             catch (DbEntityValidationException e)
             {
                 var msg = string.Empty;
-                foreach (var validationErrors in msg)
-                {
-                    foreach (var entityValidtionError in e.EntityValidationErrors)
+
+                    foreach (var validationErrors in e.EntityValidationErrors)
                     {
-                        foreach (var ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,, in eentityValidtionError)
+                        foreach (var validtionError in validationErrors.ValidationErrors)
                         {
-                            
+                            //TODO:在 Windows 环境中，C# 语言 Environment.NewLine == "\r\n" 结果为 true。
+                            msg += string.Format(
+                                $"Property:{validtionError.PropertyName} Error{validtionError.ErrorMessage}" +
+                                Environment.NewLine);
                         }
                     }
+                var fail = new Exception(msg, e);
+                throw fail;
+            }
+        }
+
+        public void Update(T entity)
+        {
+            try
+            {
+
+                    if (entity == null)
+                    {
+                        throw new ArgumentNullException(nameof(entity));
+                    }
+
+                _context.Entry(entity).State=EntityState.Modified;
+                _context.SaveChanges();
+            }
+            catch (DbEntityValidationException e)
+            {
+                var msg = string.Empty;
+
+                foreach (var validationErrors in e.EntityValidationErrors)
+                {
+                    foreach (var validtionError in validationErrors.ValidationErrors)
+                    {
+                        //TODO:在 Windows 环境中，C# 语言 Environment.NewLine == "\r\n" 结果为 true。
+                        msg += string.Format(
+                            $"Property:{validtionError.PropertyName} Error{validtionError.ErrorMessage}" +
+                            Environment.NewLine);
+                    }
                 }
+                var fail = new Exception(msg, e);
+                throw fail;
             }
         }
     }
