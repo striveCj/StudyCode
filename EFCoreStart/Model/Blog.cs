435bb3de1d826dc33f127218f7abd1a6becb0651
JackChen <530216775@qq.com>
2018-11-10 23:32:35 +0800

    Backing Fields

---
+++ EFCoreStart/Model/Blog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreStart.Model
+{
+    public class Blog
+    {
+        private Blog() { }
+
+        public Blog(string url)
+        {
+            _url = url;
+        }
+
+        private string _url;
+        public string Url => _url;
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public DateTime CreatedTime { get; set; }
+
+        public DateTime ModifiedTIme { get; set; }
+    }
+}
