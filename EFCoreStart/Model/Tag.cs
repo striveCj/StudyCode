a6834e3e0723e61cfceefff2b756f154e1b8ddae
iZg1s1p6oyu92wZ\Administrator <530216775@qq.com>
2018-11-21 21:06:16 +0800

    多对多tap类（我要买蓝牙键盘ipad打好累）

---
+++ EFCoreStart/Model/Tag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreStart.Model
+{
+  public  class Tag
+    {
+        public int TagId { get; set; }
+        public string Text { get; set; }
+
+        public ICollection<Post> Posts { get; } = new List<Post>();
+
+    }
+}
