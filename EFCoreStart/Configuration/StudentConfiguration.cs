8c6cf0e7fc259187067f39168d5b82f436b6cd6b
iZg1s1p6oyu92wZ\Administrator <530216775@qq.com>
2018-11-26 20:30:20 +0800

    一对一共享主键映射

--- EFCoreStart/Configuration/StudentConfiguration.cs
+++ EFCoreStart/Configuration/StudentConfiguration.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EFCoreStart.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,16 @@ using System.Threading.Tasks;
 
 namespace EFCoreStart.Configuration
 {
-   public class StudentConfiguration
+   public class StudentConfiguration:IEntityTypeConfiguration<Student>
     {
-
+         public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            builder.ToTable("Students");
+            builder.HasKey(k => k.Id);
+            builder.Property(p => p.Name).HasMaxLength(20).IsRequired();
+            builder.HasOne(o => o.StudentContact).WithOne(o => o.Student).HasForeignKey<StudentContact>(k => k.ContactNumber)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    
     }
 }
