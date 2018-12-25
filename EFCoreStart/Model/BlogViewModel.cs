e0eea6ec53adc33f2fdbf10509a9065a95341d26
JackChen <530216775@qq.com>
2018-12-25 21:10:48 +0800

    第三方扩展模型验证

---
+++ EFCoreStart/Model/BlogViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreStart.Model
+{
+   public class BlogViewModel
+    {
+        public int Id { get; set; }
+        public Guid TenantId { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+
+        public DateTime CreatedTime { get; set; }
+    }
+}
