af4f86c96f09d632b17106324af7126fda55c4a4
JackChen <530216775@qq.com>
2019-01-19 17:44:51 +0800

    多租户映射

---
+++ EFCoreT14/Map/BlogConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFCoreT14.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EFCoreT14.Map
+{
+    public class BlogConfiguration : IEntityTypeConfiguration<Blog>
+    {
+        public void Configure(EntityTypeBuilder<Blog> builder)
+        {
+            builder.ToTable("Blogs");
+            builder.HasKey(k => k.Id);
+        }
+    }
+}
