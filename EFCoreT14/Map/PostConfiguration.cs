45fe46dfdad5d0cfb8daa13dc26f6c0421d137fc
JackChen <530216775@qq.com>
2019-01-22 20:48:51 +0800

    增加post配置

---
+++ EFCoreT14/Map/PostConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFCoreT14.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EFCoreT14.Map
+{
+    public class PostConfiguration:IEntityTypeConfiguration<Post>
+    {
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            builder.ToTable("Posts");
+            builder.Property(k => k.Id);
+            builder.Property(p => p.Title);
+            builder.Property(p => p.Content);
+
+            builder.HasQueryFilter(p => !p.IsDeleted);
+        }
+    }
+}
