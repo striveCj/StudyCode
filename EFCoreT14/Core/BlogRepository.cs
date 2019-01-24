d462c3c59fc72c99d2a05c12a13621f79610543c
JackChen <530216775@qq.com>
2019-01-24 22:04:43 +0800

    简易仓储

---
+++ EFCoreT14/Core/BlogRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFCoreT14.Model;
+
+namespace EFCoreT14.Core
+{
+    public class BlogRepository:IBlogRepository
+    {
+        private EFCoreDbContext _context;
+
+        public BlogRepository(EFCoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IQueryable<Blog>> GetBlogs()
+        {
+            var blogs = _context.Set<Blog>();
+            return await Task.FromResult(blogs);
+        }
+    }
+}
---
+++ EFCoreT14/Core/IBlogRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFCoreT14.Model;
+
+namespace EFCoreT14.Core
+{
+    public interface IBlogRepository
+    {
+        Task<IQueryable<Blog>> GetBlogs();
+    }
+}
