668ceac909bbf56ef711e52c3347873e1fd2fbd2
ChenJie <530216775@qq.com>
2019-08-27 22:03:12 +0800

    高级编程例子

---
+++ ProfessionalCSharp3/PhoneCustomer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfessionalCSharp3
+{
+    public class PhoneCustomer
+    {
+        //常量
+        public const string DayOfSendingBill = "Monday";
+        public int CustomerID;
+        public string FirstName;
+        public string LastName;
+    }
+
+    struct PhoneCustomerStruct
+    {
+        public const string DayOfSendingBill = "Monday";
+        public int CustomerID;
+        public string FirstName;
+        public string LastName;
+    }
+
+    public class DoumentEditor
+    {
+        private static readonly uint s_maxDocuments;
+
+        static DoumentEditor()
+        {
+            s_maxDocuments = 1;
+        }
+    }
+}
---
+++ ProfessionalCSharp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfessionalCSharp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+
+        }
+
+        void T3()
+        {
+            var myCustomer=new PhoneCustomer();
+            myCustomer.FirstName = "Simon";//字段赋值
+            var myCustomer2=new PhoneCustomerStruct();
+        }
+    }
+}
