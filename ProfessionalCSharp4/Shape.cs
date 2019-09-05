876235bc1b2a9b15693e895bec95532d5d19e2f0
ChenJie <530216775@qq.com>
2019-09-05 23:33:58 +0800

    继承虚方法

---
+++ ProfessionalCSharp4/Shape.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfessionalCSharp4
+{
+    public class Shape
+    {
+        public Position Position { get; }=new Position();
+        public Size Size { get; }=new Size();
+        public virtual void Draw() => Console.WriteLine($"{Position}和{Size}");
+    }
+
+    public class Position
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    public class Size
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+
+    public class Rectangle : Shape
+    {
+        public override void Draw() => Console.WriteLine($"{Position}and{Size}");
+    
+    }
+}
