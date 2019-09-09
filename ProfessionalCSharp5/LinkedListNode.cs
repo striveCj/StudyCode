1eab69d7bb6b63c2b266c9e108b57091feb08945
ChenJie <530216775@qq.com>
2019-09-09 21:21:29 +0800

    泛型

---
+++ ProfessionalCSharp5/LinkedListNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfessionalCSharp5
+{
+    public class LinkedListNode
+    {
+        public LinkedListNode(object value) => Value = value;
+        public  object Value { get; }
+        public LinkedListNode Next { get; internal set; }
+        public LinkedListNode Prev { get; internal set; }
+    }
+}
---
+++ ProfessionalCSharp5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfessionalCSharp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new ArrayList();
+            list.Add(44);
+            int t1 = (int) list[0];
+            foreach (var i2 in list)
+            {
+                Console.WriteLine(i2);
+            }
+            var list2 = new List<int>();
+            list.Add(44);
+            int t2 = list2[0];
+            foreach (var i2 in list2)
+            {
+                Console.WriteLine(i2);
+            }
+
+        }
+    }
+}
