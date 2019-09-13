591054d1e1dcdadfbd8b68a61ab8cc5fc9ce3387
ChenJie <530216775@qq.com>
2019-09-13 23:07:01 +0800

    泛型类

---
+++ ProfessionalCSharp5/DocmentManagerT.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfessionalCSharp5
+{
+       public   class DocmentManagerT<T>
+    {
+
+        private  readonly  Queue<T> _docmentQueue=new Queue<T>();
+        private  readonly  object _lockQueue=new object();
+
+        public void ASddDocument(T doc)
+        {
+            lock (_lockQueue)
+            {
+                _docmentQueue.Enqueue(doc);
+            }
+        }
+
+        public bool IsDocumentAvailable => _docmentQueue.Count > 0;
+    }
+}
