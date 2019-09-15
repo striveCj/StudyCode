43c6b9805069939af421051c737e2dde84fabb9b
ChenJie <530216775@qq.com>
2019-09-15 22:19:28 +0800

    泛型接口协变

---
+++ ProfessionalCSharp5/IIndex.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfessionalCSharp5
+{
+    public class Shape
+    {
+        public double Width { get; set; }
+        public  double Height { get; set; }
+        public override string ToString()
+        {
+            return $"Width:{Width},Height:{Height}";
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        
+    }
+
+   public interface IIndex<out T>
+    {
+        T this[int indx] { get; }
+        int Count { get; }
+    }
+
+    public class RectangleCollection : IIndex<Rectangle>
+    {
+        private Rectangle[] data=new Rectangle[3]
+        {
+            new Rectangle{Height = 2,Width = 5},
+            new Rectangle{Height = 3,Width = 7},
+            new Rectangle{Height = 4.5,Width=2.9}
+        };
+
+        private static RectangleCollection _coll;
+        public static RectangleCollection GetRectangles => _coll ?? (_coll = new RectangleCollection());
+
+        public Rectangle this[int index]
+        {
+            get
+            {
+                if (index<0||index>data.Length)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index));
+                   
+                }
+                return data[index];
+            }
+        }
+
+        public int Count => data.Length;
+    }
+}
