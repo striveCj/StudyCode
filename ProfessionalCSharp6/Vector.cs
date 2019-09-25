b54a9790cd19a52d80819fee1d749d05aacf7b60
ChenJie <530216775@qq.com>
2019-09-25 21:04:33 +0800

    运算符重写2

--- ProfessionalCSharp6/Vector.cs
+++ ProfessionalCSharp6/Vector.cs
@@ -21,6 +21,25 @@ namespace ProfessionalCSharp6
         public override string ToString() => $"({X},{Y},{Z})";
 
         public static Vector operator +(Vector left,Vector right)=>new Vector(left.X+right.X,left.Y+right.Y,left.Z+right.Z);
+        public static Vector operator *(double left, Vector right) => new Vector(left *right.X, left*right.Y, left* right.Z);
+        public static Vector operator *(Vector left, double right) => new Vector(left.X * right, left.Y * right, left.Z * right);
+        public static Vector operator *(Vector left, Vector right) => new Vector(left.X * right.X, left.Y * right.Y, left.Z * right.Z);
+
+        public static bool operator ==(Vector left, Vector right)
+        {
+            if (object.ReferenceEquals(left, right)) return true;
+            return left.X == right.X && left.Y == right.Y && left.Z == right.Z;
+        }
+
+        public static bool operator !=(Vector left, Vector right) => !(right == left);
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            return this == (Vector) obj;
+        }
+
+        public override int GetHashCode() => X.GetHashCode() ^ (Y.GetHashCode() ^ Z.GetHashCode());
 
     } 
 
