86715cdcbde98e50b1e37528f6512d79816e2121
ChenJie <530216775@qq.com>
2019-09-26 21:28:58 +0800

    实现自定义的索引运算符

---
+++ ProfessionalCSharp6/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfessionalCSharp6
+{
+   public class Person
+    {
+        public DateTime Birthday { get; }
+        public string FirstName { get; }
+
+        public string LastName { get; }
+        public Person(string firstName,string lastName, DateTime birthDay)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Birthday = birthDay;
+        }
+
+        public override string ToString() => $"{FirstName}{LastName}";
+    }
+
+    public class PersonCollection
+    {
+        private Person[] _people;
+        public PersonCollection(params Person[] people) => _people = people.ToArray();
+
+        public Person this[int index]
+        {
+            get => _people[index];
+            set => _people[index] = value;
+        }
+    }
+}
