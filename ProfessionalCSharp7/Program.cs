b97d2eb8fa184d70da63fe9c8118f95c2a40904a
ChenJie <530216775@qq.com>
2019-10-07 21:08:22 +0800

    枚举

--- ProfessionalCSharp7/Program.cs
+++ ProfessionalCSharp7/Program.cs
@@ -10,37 +10,57 @@ namespace ProfessionalCSharp7
     {
         static void Main(string[] args)
         {
-            string[] names =
-            {
-                "Christina Aguilera",
-                "Shakira",
-                "Beyonce",
-                "Lady Gaga"
-            };
-            Array.Sort(names);
-            foreach (var name in names)         {
-                Console.WriteLine(names);
-            }
+            //string[] names =
+            //{
+            //    "Christina Aguilera",
+            //    "Shakira",
+            //    "Beyonce",
+            //    "Lady Gaga"
+            //};
+            //Array.Sort(names);
+            //foreach (var name in names)         {
+            //    Console.WriteLine(names);
+            //}
 
-            Person[] beatles =
-            {
-                new Person {FirstName = "a", LastName = "b"},
-                new Person {FirstName = "a", LastName = "b"}
-            };
-            Person[] batelesClone = (Person[]) beatles.Clone();
+            //Person[] beatles =
+            //{
+            //    new Person {FirstName = "a", LastName = "b"},
+            //    new Person {FirstName = "a", LastName = "b"}
+            //};
+            //Person[] batelesClone = (Person[]) beatles.Clone();
 
-            Person[] persons =
+            //Person[] persons =
+            //{
+            //    new Person("Damon", "Hill"),
+            //    new Person("Niki", "Lauda"),
+            //    new Person("Ayrton", "Senna"),
+            //    new Person("Graham", "Hill"),
+            //};
+            //Array.Sort(persons);
+            //foreach (var p in persons)
+            //{
+            //    Console.WriteLine(p);
+            //}
+
+
+        }
+
+        public void HelloWorld()
+        {
+            var helloCollection=new HellCollection();
+            foreach (var s in helloCollection)
             {
-                new Person("Damon", "Hill"),
-                new Person("Niki", "Lauda"),
-                new Person("Ayrton", "Senna"),
-                new Person("Graham", "Hill"),
-            };
-            Array.Sort(persons);
-            foreach (var p in persons)
-            {
-                Console.WriteLine(p);
+                Console.WriteLine(s);
             }
+        }
+    }
+
+    public class HellCollection
+    {
+        public IEnumerator<string> GetEnumerator()
+        {
+            yield return "Hello";
+            yield return "World";
         }
     }
 }
