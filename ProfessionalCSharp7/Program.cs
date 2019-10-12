6e15224a578731ced8409d55a3793cbb9eca66f3
ChenJie <530216775@qq.com>
2019-10-12 22:56:50 +0800

    迭代器

--- ProfessionalCSharp7/Program.cs
+++ ProfessionalCSharp7/Program.cs
@@ -143,6 +143,46 @@ namespace ProfessionalCSharp7
             }
         }
     }
+
+    public class GameMoves
+    {
+        private IEnumerator _cross;
+        private IEnumerator _circle;
+
+        public GameMoves()
+        {
+           
+        }
+
+        private int _move = 0;
+        private const int MaxMoves = 9;
+        public IEnumerator Cross()
+        {
+            while (true)
+            {
+                Console.WriteLine($"Cross,move{_move}");
+                if (++_move>=MaxMoves)
+                {
+                    yield break;
+                }
+                yield return _circle;
+            }
+
+        }
+
+        public IEnumerator Cicle()
+        {
+            while (true)
+            {
+                Console.WriteLine($"Circle,move{_move}");
+                if (++_move>=MaxMoves)
+                {
+                    yield break;
+                }
+                yield return _cross;
+            }
+        }
+    }
   
 
 }
