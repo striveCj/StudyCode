3b5be70cd02495a5f4fe3208ec19e7171123e1ee
ChenJie <530216775@qq.com>
2019-10-23 21:50:10 +0800

    委托判断

---
+++ ProfessionalCSharp8/Currency.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfessionalCSharp8
+{
+   public class Currency
+   {
+       public uint Dollars;
+       public ushort Cents;
+
+       public Currency(uint dollars, ushort cents)
+       {
+           Dollars = dollars;
+           Cents = cents;
+       }
+
+       public override string ToString()
+       {
+           return $"{Dollars}.{Cents,2:00}";
+       }
+
+       public static string GetCurrencyUnit() => "Dollar";
+
+       public static explicit operator Currency(float value)
+       {
+           checked
+           {
+               uint dollars = (uint) value;
+               ushort cents = (ushort) ((value - dollars) * 100);
+                return new Currency(dollars,cents);
+           }
+       }
+
+       public static implicit operator float(Currency value) => value.Dollars + (value.Cents / 100.0f);
+        public static implicit operator  Currency(uint value)=>new Currency(value,0);
+       public static implicit operator uint(Currency value) => value.Dollars;
+   }
+}
