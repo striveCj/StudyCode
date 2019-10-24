eb31b2f736087867b2be9251b6b20693ee16395c
ChenJie <530216775@qq.com>
2019-10-24 22:25:14 +0800

    使用委托

--- ProfessionalCSharp8/Program.cs
+++ ProfessionalCSharp8/Program.cs
@@ -10,9 +10,18 @@ namespace ProfessionalCSharp8
     {
         static void Main(string[] args)
         {
+            //int x = 40;
+            //GetAString firstStringMethod=new GetAString(x.ToString);
+            //Console.WriteLine($"String is {firstStringMethod}");
+
             int x = 40;
-            GetAString firstStringMethod=new GetAString(x.ToString);
-            Console.WriteLine($"String is {firstStringMethod}");
+            GetAString firstStringMethod = x.ToString;
+            Console.WriteLine($"{firstStringMethod}");
+            var balance=new Currency(34,50);
+            firstStringMethod = balance.ToString;
+            Console.WriteLine($"{firstStringMethod}");
+            firstStringMethod=new GetAString(Currency.GetCurrencyUnit);
+            Console.WriteLine($"{firstStringMethod}");
         }
 
         private delegate string GetAString();
