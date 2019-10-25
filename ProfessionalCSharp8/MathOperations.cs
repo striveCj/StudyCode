eb1acdf19290bf274bfbaceaa02fd71fac0ceade
ChenJie <530216775@qq.com>
2019-10-25 23:54:51 +0800

    委托

---
+++ ProfessionalCSharp8/MathOperations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfessionalCSharp8
+{
+    class MathOperations
+    {
+        public static double MultiplyByTwo(double value) => value * 2;
+        public static double Square(double value) => value * value;
+    }
+}
