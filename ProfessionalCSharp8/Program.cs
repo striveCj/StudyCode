a4fbdeec31df64f0d58dc1d93b6bd83f0bfa82a2
ChenJie <530216775@qq.com>
2019-10-26 21:06:47 +0800

    委托

--- ProfessionalCSharp8/Program.cs
+++ ProfessionalCSharp8/Program.cs
@@ -14,16 +14,37 @@ namespace ProfessionalCSharp8
             //GetAString firstStringMethod=new GetAString(x.ToString);
             //Console.WriteLine($"String is {firstStringMethod}");
 
-            int x = 40;
-            GetAString firstStringMethod = x.ToString;
-            Console.WriteLine($"{firstStringMethod}");
-            var balance=new Currency(34,50);
-            firstStringMethod = balance.ToString;
-            Console.WriteLine($"{firstStringMethod}");
-            firstStringMethod=new GetAString(Currency.GetCurrencyUnit);
-            Console.WriteLine($"{firstStringMethod}");
+            //int x = 40;
+            //GetAString firstStringMethod = x.ToString;
+            //Console.WriteLine($"{firstStringMethod}");
+            //var balance=new Currency(34,50);
+            //firstStringMethod = balance.ToString;
+            //Console.WriteLine($"{firstStringMethod}");
+            //firstStringMethod=new GetAString(Currency.GetCurrencyUnit);
+            //Console.WriteLine($"{firstStringMethod}");
+
+            DoubleOp[] operations =
+            {
+                MathOperations.MultiplyByTwo,
+                MathOperations.Square
+            };
+            for (int i = 0; i < operations.Length; i++)
+            {
+                Console.WriteLine($"Using operations[{i}]");
+                ProcessAndDisplayNumber(operations[i], 2.0);
+                ProcessAndDisplayNumber(operations[i], 7.94);
+                ProcessAndDisplayNumber(operations[i],1.414);
+                Console.WriteLine();
+            }
         }
 
+        static void ProcessAndDisplayNumber(DoubleOp action, double value)
+        {
+            double result = action(value);
+            Console.WriteLine($"Value is {value},result of operation is {result}");
+        }
         private delegate string GetAString();
+
+        delegate double DoubleOp(double x);
     }
 }
