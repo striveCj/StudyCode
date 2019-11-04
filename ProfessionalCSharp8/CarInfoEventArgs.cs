51a0ebb4d2f42194fb37ff67b51ac481a941036c
ChenJie <530216775@qq.com>
2019-11-04 22:53:13 +0800

    事件发布

---
+++ ProfessionalCSharp8/CarInfoEventArgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Console = System.Console;
+
+namespace ProfessionalCSharp8
+{
+   public class CarInfoEventArgs:EventArgs
+   {
+       public CarInfoEventArgs(string car) => Car = car;
+        public  string Car { get; set; }
+   }
+
+    public class CarDealer
+    {
+        public event EventHandler<CarInfoEventArgs> NewCarInfo;
+
+        public void NewCar(string car)
+        {
+            Console.WriteLine($"CarDealer,new car{car}");
+            NewCarInfo?.Invoke(this, new CarInfoEventArgs(car));
+        }
+    }
+}
