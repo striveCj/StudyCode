cd2a232da99c7e93035d4514d6c650ce706d77c5
ChenJie <530216775@qq.com>
2019-11-14 22:08:00 +0800

    列表

---
+++ ProfessionalCSharp10/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfessionalCSharp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Racer : IComparable<Racer>, IFormattable
+    {
+        public int Id { get; }
+
+        public string FirstName { get; }
+
+        public string LastName { get; }
+
+        public string Country { get; }
+
+        public  int Wins { get; }
+
+        public Racer(int id,string firstName,string lastName,string country) :this(id,firstName,lastName,country, wins: 0){ }
+
+        public Racer(int id, string firstName, string lastName, string country,int wins)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Country = country;
+            Wins = wins;
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CompareTo(Racer other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
