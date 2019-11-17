c1aa6b36243d49b662f3b6a49ac899db00034d93
ChenJie <530216775@qq.com>
2019-11-17 23:06:52 +0800

    add List

--- ProfessionalCSharp10/Program.cs
+++ ProfessionalCSharp10/Program.cs
@@ -15,6 +15,21 @@ namespace ProfessionalCSharp10
             List<int> intList=new List<int>(10);
             intList.Capacity = 20;
             intList.TrimExcess();
+            var intList2=new List<int>();
+            intList2.Add(1);
+            intList2.Add(2);
+            var stringList=new List<string>();
+            stringList.Add("one");
+            stringList.Add("two");
+
+            var  graham=new Racer(7,"Graham","Hill","UK",14);
+            var emerson = new Racer(7, "Graham", "Hill", "UK", 14);
+            var mario = new Racer(7, "Graham", "Hill", "UK", 14);
+            var racers2 = new List<Racer>(20){ graham,emerson,mario};
+            racers2.Add(new Racer(24,"a","b","c",25));
+            racers2.Add(new Racer(21,"a","b","c",22));
+
+
         }
     }
 
