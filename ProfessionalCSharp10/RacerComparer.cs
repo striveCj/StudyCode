9f29ffe6e6300f49d05ee29b52ba3e8983c241d4
ChenJie <530216775@qq.com>
2019-11-19 22:04:29 +0800

    排序

---
+++ ProfessionalCSharp10/RacerComparer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfessionalCSharp10
+{
+    public class RacerComparer:IComparable<Racer>
+    {
+        public enum  CompareType
+        {
+            FirstName,
+            LastName,
+            Country,
+            Wins
+        }
+        private  CompareType  _compareType;
+
+        public RacerComparer(CompareType compareType)
+        {
+            _compareType = compareType;
+        }
+
+        public int Compare(Racer x, Racer y)
+        {
+            if (x==null&&y==null)
+            {
+                return 0;
+            }
+            if (x==null)
+            {
+                return -1;
+            }
+            if (y==null)
+            {
+                return 1;
+            }
+            int result;
+            switch (_compareType)
+            {
+                case CompareType.FirstName:
+                    return string.Compare(x.FirstName, y.FirstName);
+                case CompareType.LastName:
+                    return string.Compare(x.LastName, y.LastName);
+                case CompareType.Country:
+                    result = string.Compare(x.LastName, y.LastName);
+                    if (result==0)
+                    {
+                        return string.Compare(x.LastName, y.LastName);
+                    }
+                    else
+                    {
+                        return result;
+                    }
+                case CompareType.Wins:
+                    return x.Wins.CompareTo(y.Wins);
+                default:
+                    throw new ArgumentException("Invalid Compare Type");
+            }
+        }
+
+        public int CompareTo(Racer other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
