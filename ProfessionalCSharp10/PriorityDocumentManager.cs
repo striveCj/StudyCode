020d1d6ec3ddbc5ffcef6ac9378eb3cd7e76a412
ChenJie <530216775@qq.com>
2019-11-26 21:43:30 +0800

    链表

--- ProfessionalCSharp10/PriorityDocumentManager.cs
+++ ProfessionalCSharp10/PriorityDocumentManager.cs
@@ -9,16 +9,30 @@ namespace ProfessionalCSharp10
     public class PriorityDocumentManager
     {
         private readonly LinkedList<Document> _documentList;
-        private readonly List<LinkedListNode<Double>> _priorityNodes;
+        private readonly List<LinkedListNode<Document>> _priorityNodes;
 
         public PriorityDocumentManager()
         {
             _documentList=new LinkedList<Document>();
-            _priorityNodes=new List<LinkedListNode<double>>(10);
+            _priorityNodes=new List<LinkedListNode<Document>>(10);
             for (int i = 0; i < 10; i++)
             {
                 _priorityNodes.Add(new LinkedListNode<Document>(null));
             }
+        }
+
+        public void AddDocument(Document d)
+        {
+            if (d==null)
+            {
+                throw new ArgumentNullException(nameof(d));
+            }
+            AddDocmentToPriorityNode(d,d.Priority);
+        }
+
+        public void AddDocmentToPriorityNode(Document doc, int priority)
+        {
+            
         }
     }
 }
