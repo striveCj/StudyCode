40d1e3e9bb9a892aa8332cf489deb21289b72493
ChenJie <530216775@qq.com>
2019-12-02 21:57:27 +0800

    字典

---
+++ ProfessionalCSharp10/EmployeeIdException.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfessionalCSharp10
+{
+    public class EmployeeIdException:Exception
+    {
+        public EmployeeIdException(string message) : base(message) { }
+    }
+
+    public struct EmployeeId : IEquatable<EmployeeId>
+    {
+        private readonly char _prefix;
+        private readonly int _number;
+
+        public EmployeeId(string id)
+        {
+            if (id==null)throw new ArgumentNullException(nameof(id));
+            _prefix = (id.ToUpper())[0];
+            int numLength = id.Length - 1;
+            try
+            {
+                _number = int.Parse(id.Substring(1,numLength>6?6:numLength));
+            }
+            catch (Exception e)
+            {
+                
+                throw new EmployeeIdException("请格式化");
+            }
+        }
+
+        public override string ToString() => _prefix.ToString()+$"{_number,6:00000}";
+        public bool Equals(EmployeeId other)
+        {
+            return _prefix == other._prefix && _number == other._number;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is EmployeeId && Equals((EmployeeId) obj);
+        }
+
+        public override int GetHashCode() => (_number ^ _number << 16) * 0x15051505;
+       
+    }
+}
