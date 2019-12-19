81f00f7e95c695ab1225a83ca722ba5f68fc97c6
ChenJie <530216775@qq.com>
2019-12-19 22:26:39 +0800

    1发出异步的Get请求

--- ProfessionalCSharp23/Program.cs
+++ ProfessionalCSharp23/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,7 +15,26 @@ namespace ProfessionalCSharp23
 
         #region 23.2.1发出异步的Get请求
 
-        
+        private const string NorthwindUrl = "http://services.data.org/Northwind/Northwind.svc/Regions";
+
+        private const  string IncorrectUrl= "http://services.data.org/Northwind1/Northwind.svc/Regions";
+
+        private HttpClient _httpClient;
+
+        public HttpClient HttpClient => _httpClient ?? (_httpClient = new HttpClient());
+
+        private async Task GetDataSimpleAsync()
+        {
+            HttpResponseMessage responese = await HttpClient.GetAsync(NorthwindUrl);
+            if (responese.IsSuccessStatusCode)
+            {
+                Console.WriteLine($"{(int)responese.StatusCode}{responese.ReasonPhrase}");
+                string respomseBpdyAsText = await responese.Content.ReadAsStringAsync();
+                Console.WriteLine($"Received payload of{respomseBpdyAsText.Length}characters ");
+                Console.WriteLine();
+                Console.WriteLine(respomseBpdyAsText);
+            }
+        }
 
         #endregion
     }
