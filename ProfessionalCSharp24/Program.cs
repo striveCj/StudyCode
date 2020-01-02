76f0a8348db8e9e218c7b5b7b1a46cfecd08753f
ChenJie <530216775@qq.com>
2020-01-02 22:57:10 +0800

    验证用户信息

---
+++ ProfessionalCSharp24/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.DesignerServices;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfessionalCSharp24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static WindowsIdentity ShowIdentityInformation()
+        {
+            WindowsIdentity identity=WindowsIdentity.GetCurrent();
+            if (identity==null)
+            {
+                Console.WriteLine("not a windows identity");
+                return null;
+            }
+        }
+    }
+}
