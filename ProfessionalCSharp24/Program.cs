c0c1b6ebcaea15d0c5f4e7f2e38ae8bb2ea5c159
ChenJie <530216775@qq.com>
2020-01-25 23:38:44 +0800

    .

--- ProfessionalCSharp24/Program.cs
+++ ProfessionalCSharp24/Program.cs
@@ -92,7 +92,7 @@ namespace ProfessionalCSharp24
         private void InitAliceKeys()
         {
             _aliceKeySignature=CngKey.Create(CngAlgorithm.Sha512);
-            _alicePubKeyBlob = _aliceKeySignature.Export(CngKeyBlobFormat.GenericPublicBlob);；
+            _alicePubKeyBlob = _aliceKeySignature.Export(CngKeyBlobFormat.GenericPublicBlob);
         }
 
         private void InitAliceKeysRsa()
