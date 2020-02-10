36114eace259d3459afe46fdc7bd017c394e5e6b
ChenJie <530216775@qq.com>
2020-02-10 23:52:58 +0800

    构造函数

--- ProfessionalCSharp12/Racer.cs
+++ ProfessionalCSharp12/Racer.cs
@@ -6,6 +6,14 @@ namespace ProfessionalCSharp12
 {
     public class Racer : IComparable<Racer>, IFormattable
     {
+        public Racer(string firstName, string lastName, string country, int starts, int wins) : this(firstName, lastName, country, starts, wins, null, null)
+        {
+
+        }
+        public Racer(string firstName,string lastName,string country, int satarts, int wins, IEnumerable<int> years, IEnumerable<string> cars) {
+
+        }
+
         public int CompareTo(Racer other)
         {
             throw new NotImplementedException();
