6119320c662c96415af10cbe51c858614f86a5c1
ChenJie <530216775@qq.com>
2020-02-13 23:09:54 +0800

    Team

---
+++ ProfessionalCSharp12/Team.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProfessionalCSharp12
+{
+   public class Team
+    {
+        public Team(string name,params int[] years)
+        {
+            Name = name;
+            Years = Years != null ? new List<int>(years) : new List<int>();
+
+        }
+        public string Name { get; }
+        public IEnumerable<int> Years { get; }
+    }
+}
