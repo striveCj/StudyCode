5c068a9178f0d9daf9090894b0b191dd127d4ee4
ChenJie <530216775@qq.com>
2020-02-14 23:54:56 +0800

    Formulal

---
+++ ProfessionalCSharp12/Formulal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProfessionalCSharp12
+{
+    public class Formulal
+    {
+        private static List<Racer> s_racers;
+        public static IList<Racer> GetChampions() => s_racers;
+    }
+}
