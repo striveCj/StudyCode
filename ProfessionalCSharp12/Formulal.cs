c4e99ac0b3ddf629f1326213c962e21fadf9f59c
ChenJie <530216775@qq.com>
2020-02-15 23:30:34 +0800

    InitializeRacers

--- ProfessionalCSharp12/Formulal.cs
+++ ProfessionalCSharp12/Formulal.cs
@@ -7,6 +7,17 @@ namespace ProfessionalCSharp12
     public class Formulal
     {
         private static List<Racer> s_racers;
-        public static IList<Racer> GetChampions() => s_racers;
+        public static IList<Racer> GetChampions() => s_racers?? InitializeRacers();
+
+        private static List<Racer> InitializeRacers()
+        {
+            return new List<Racer>
+        {
+            new Racer("Nino","Farina","Italy",33,5,new int[]{1950},new string[] {"Alfa Rome" }),
+             new Racer("Nino1","Farina1","Italy1",33,5,new int[]{1950},new string[] {"Alfa Rome1" }),
+              new Racer("Nino2","Farina2","Italy2",33,5,new int[]{1950},new string[] {"Alfa Rome2" }),
+        };
+        }
+
     }
 }
