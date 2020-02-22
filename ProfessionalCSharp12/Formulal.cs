89dd0e629aa8e81faef98c78bbf49949bb947d81
ChenJie <530216775@qq.com>
2020-02-22 23:37:49 +0800

    ；

--- ProfessionalCSharp12/Formulal.cs
+++ ProfessionalCSharp12/Formulal.cs
@@ -77,7 +77,7 @@ namespace ProfessionalCSharp12
             Console.WriteLine("First iteration");
             foreach (string name in namesWithJ)
             {
-                Console.WriteLine(name);
+                Console.WriteLine(name);；
             }
 
         }
