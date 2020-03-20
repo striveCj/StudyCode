c5a84daeb728643f365389eb147aa5fb533939f6
ChenJie <530216775@qq.com>
2020-03-21 00:49:02 +0800

    AggregateCount

--- ProfessionalCSharp12/LinqSamples.cs
+++ ProfessionalCSharp12/LinqSamples.cs
@@ -17,8 +17,25 @@ namespace ProfessionalCSharp12
                 var racers = (from r in Formulal.GetChampions() orderby r.LastName, r.FirstName select r.FirstName + " " + r.LastName).Skip(pageSize * pageSize).Take(pageSize);
                 foreach (var item in racers)
                 {
-                    Console.WriteLine(item);；
+                    Console.WriteLine(item);
                 }
+            }
+        }
+
+        static void AggregateCount()
+        {
+            var query = from r in Formulal.GetChampions()
+                        let numberYears = r.Years.Count()
+                        where numberYears >= 3
+                        orderby numberYears descending, r.LastName
+                        select new
+                        {
+                            Name = r.FirstName + " " + r.LastName,
+                            TimeChampion = numberYears
+                        };
+            foreach (var item in query)
+            {
+                Console.WriteLine($"{item.Name}{item.TimeChampion}");
             }
         }
     }
