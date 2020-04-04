b3f69d03498d8224c386e1c381af52aab14adfa4
ChenJie <530216775@qq.com>
2020-04-04 23:51:53 +0800

    IntroWithLocalFunctionWithClosures

--- ProfessionalCSharp13/Program.cs
+++ ProfessionalCSharp13/Program.cs
@@ -32,5 +32,34 @@ namespace ProfessionalCSharp13
             int result = add(37, 5);
             Console.WriteLine(result);
         }
+
+        private static void IntroWithLocalFunctionWithClosures()
+        {
+            int z = 3;
+            int result = add(37, 5);
+            Console.WriteLine(result);
+
+            int add(int x, int y) => x + y + z;
+        }
+
+        private static IEnumerable<T> Where1<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            if (source==null)
+            {
+                throw new ArgumentException(nameof(source));
+            }
+            if (predicate==null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+
+            foreach (T item in source)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
     }
 }
