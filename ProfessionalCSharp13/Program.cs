7baa24688f55709128215d91d8e3e2ed7cd9787c
ChenJie <530216775@qq.com>
2020-04-09 23:37:32 +0800

    QuickSort

--- ProfessionalCSharp13/Program.cs
+++ ProfessionalCSharp13/Program.cs
@@ -142,5 +142,36 @@ namespace ProfessionalCSharp13
                 }
             }
         }
+
+        private static void QuickSort<T>(T[] elements) where T : IComparable<T>
+        {
+            void Sort(int start,int end)
+            {
+                int i = start, j = end;
+                var pivot = elements[(start + end) / 2];
+                while (i<=j)
+                {
+                    while (elements[i].CompareTo(pivot) < 0) i++;
+                    while (elements[i].CompareTo(pivot) > 0) j--;
+                    if (i<=j)
+                    {
+                        T tmp = elements[i];
+                        elements[i] = elements[j];
+                        elements[j] = tmp;
+                        i++;
+                        j--;
+                    }
+                }
+                if (start<j)
+                {
+                    Sort(start, j);
+                }
+                if (i<end)
+                {
+                    Sort(i, end);
+                }
+            }
+            Sort(0, elements.Length - 1);
+        }
     }
 }
