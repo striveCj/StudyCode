550363d525c696b4a3ffcc8c6104ffaeeabd8807
ChenJie <530216775@qq.com>
2020-04-15 23:36:23 +0800

    Divide

---
+++ ProfessionalCSharp13/SimpleMath.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfessionalCSharp13
+{
+    public class SimpleMath
+    {
+        [return TupleElementNames(new string[] { "result","remainder"})]
+        public static ValueTuple<int, int> Divide(int dividend, int divisor);
+        public static (int result,int remainder) Divide(int dividend,int divisor)
+        {
+            int result = dividend / divisor;
+            int remainder = dividend % divisor;
+            return (result, remainder);
+        }
+
+    }
+}
