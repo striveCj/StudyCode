ceb4b723d8a54a5135bc90a1c1ea172ba4676642
ChenJie <530216775@qq.com>
2020-04-18 00:13:53 +0800

    TupleNames

--- ProfessionalCSharp13/SimpleMath.cs
+++ ProfessionalCSharp13/SimpleMath.cs
@@ -8,8 +8,7 @@ namespace ProfessionalCSharp13
 {
     public class SimpleMath
     {
-        [return TupleElementNames(new string[] { "result","remainder"})]
-        public static ValueTuple<int, int> Divide(int dividend, int divisor);
+
         public static (int result,int remainder) Divide(int dividend,int divisor)
         {
             int result = dividend / divisor;
@@ -19,7 +18,19 @@ namespace ProfessionalCSharp13
         private static void UseALibrary()
         {
             var t = SimpleMath.Divide(5, 3);
-            Console.WriteLine($"{t.result},{t.remainder}");；
+            Console.WriteLine($"{t.result},{t.remainder}");
+        }
+
+        private static void TupleNames()
+        {
+            var t1 = Divide(9, 4);
+            Console.WriteLine( $"{t1.result},{t1.remainder}");
+            (int res, int rem) t2 = Divide(11, 3);
+            Console.WriteLine($"{t2.res}{t2.rem}");
+
+            var t3 = (res: t1.result, rem: t1.remainder);
+            var t4 = (t1.result, t1.remainder);
+            Console.WriteLine($"{t4.result},{t4.remainder}");
         }
     }
 }
