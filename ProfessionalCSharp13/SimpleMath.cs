73c9d480ce0d8bbb55ca2c5332b033138ea6dd75
ChenJie <530216775@qq.com>
2020-04-18 23:57:21 +0800

    TuplesWithLinkedList

--- ProfessionalCSharp13/SimpleMath.cs
+++ ProfessionalCSharp13/SimpleMath.cs
@@ -9,7 +9,7 @@ namespace ProfessionalCSharp13
     public class SimpleMath
     {
 
-        public static (int result,int remainder) Divide(int dividend,int divisor)
+        public static (int result, int remainder) Divide(int dividend, int divisor)
         {
             int result = dividend / divisor;
             int remainder = dividend % divisor;
@@ -24,13 +24,27 @@ namespace ProfessionalCSharp13
         private static void TupleNames()
         {
             var t1 = Divide(9, 4);
-            Console.WriteLine( $"{t1.result},{t1.remainder}");
+            Console.WriteLine($"{t1.result},{t1.remainder}");
             (int res, int rem) t2 = Divide(11, 3);
             Console.WriteLine($"{t2.res}{t2.rem}");
 
             var t3 = (res: t1.result, rem: t1.remainder);
             var t4 = (t1.result, t1.remainder);
             Console.WriteLine($"{t4.result},{t4.remainder}");
+        }
+
+        static void TuplesWithLinkedList()
+        {
+            Console.WriteLine(nameof(TuplesWithLinkedList));
+            var list = new LinkedList<int>(Enumerable.Range(0, 10));
+            int value;
+            LinkedListNode<int> node = list.First;
+            do
+            {
+                (value, node) = (node.Value, node.Next);
+                Console.WriteLine(value);
+            } while (node != null);
+            Console.WriteLine();
         }
     }
 }
