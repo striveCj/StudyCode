e05c071e721dd718106bf60be2862a53909dc217
ChenJie <530216775@qq.com>
2020-04-25 23:09:05 +0800

    NumberDemol

---
+++ ProfessionalCSharp14/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfessionalCSharp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void NumberDemol(string n)
+        {
+            if (n is null)
+            {
+                throw new ArgumentNullException(nameof(n));
+            }
+            try
+            {
+                int i = int.Parse(n);
+                Console.WriteLine($"{i}");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }          
+        }
+
+    }
+}
