756e2e3a0148b463ba69c3c42a0746eeab940e4e
ChenJie <530216775@qq.com>
2020-05-19 21:38:22 +0800

    反射

--- ProfessionalCSharp16/Program.cs
+++ ProfessionalCSharp16/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,31 @@ namespace ProfessionalCSharp16
 {
     class Program
     {
+        private static StringBuilder OutputText = new StringBuilder();
         static void Main(string[] args)
         {
         }
+
+        static void AnalyzeType(Type t)
+        {
+            TypeInfo typeInfo = t.GetTypeInfo();
+            AddToOutput(t.Name);
+            AddToOutput(t.FullName);
+            AddToOutput(t.Namespace);
+
+            Type tBase = t.BaseType;
+            if (tBase!=null)
+            {
+                AddToOutput(tBase.Name);
+            }
+            AddToOutput("\npublic members:");
+            foreach (var item in t.GetMembers())
+            {
+                AddToOutput($"{item.DeclaringType}{item.MemberType}{item.Name}");
+            }
+        
+        }
+
+        static void AddToOutput(string Text) => OutputText.Append("\n" + Text);
     }
 }
