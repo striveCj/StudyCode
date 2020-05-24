536ad68bada53ea43065c73b7d8d1792533df3e8
ChenJie <530216775@qq.com>
2020-05-24 23:51:30 +0800

    DynamicObject

---
+++ ProfessionalCSharp16/WroxDynamicObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfessionalCSharp16
+{
+    public class WroxDynamicObject:DynamicObject
+    {
+        private Dictionary<string, object> _dynamicData = new Dictionary<string, object>();
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            return base.TryGetMember(binder, out result);
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            return base.TrySetMember(binder, value);
+        }
+
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+        {
+            return base.TryInvokeMember(binder, args, out result);
+        }
+    }
+}
