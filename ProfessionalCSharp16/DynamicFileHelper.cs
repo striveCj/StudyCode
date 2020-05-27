d7dff50b7135fd97c6f71dbd24dd5c6364ac3e77
ChenJie <530216775@qq.com>
2020-05-27 23:21:48 +0800

    DynamicFileHelper

---
+++ ProfessionalCSharp16/DynamicFileHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfessionalCSharp16
+{
+    public class DynamicFileHelper
+    {
+        private StreamReader OpenFile(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                return new StreamReader(fileName);
+            }
+            return null;
+        }
+
+        public IEnumerable<dynamic> ParseFile(string fileName)
+        {
+            string[] headerLine = fileStream.ReadLine().Split(',').Trim().ToArray();
+            var retList = new List<dynamic>();
+            while (fileStream.Peek()>0)
+            {
+                string[] dataLine = fileStream.ReadLine().Split(',').Trim().ToArray();
+                dynamic dynamicEntity = new ExpandoObject();
+                for (int i = 0; i < headerLine.length; i++)
+                {
+                    ((IDictionary<string, object>)dynamicEntity).Add(headerLine[i],dataLine[i]);
+                }
+                retList.Add(dynamicEntity);
+            }
+            return retList;
+        }
+    }
+}
