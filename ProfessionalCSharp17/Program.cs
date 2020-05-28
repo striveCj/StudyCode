c247690e26afa511bce8fb405f03efdc2a49016c
ChenJie <530216775@qq.com>
2020-05-28 23:29:23 +0800

    析构函数

---
+++ ProfessionalCSharp17/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ProfessionalCSharp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public class ResourceHolder : IDisposable
+        {
+            private bool _isDisposed = false;
+            public void Dispose()
+            {
+                Dispose(true);
+                GC.SuppressFinalize(this);
+            }
+
+            protected virtual void Dispose(bool disposing)
+            {
+                if (!_isDisposed)
+                {
+                    if (disposing)
+                    {
+
+                    }
+                }
+                _isDisposed = true;
+            }
+            ~ResourceHolder()
+            {
+                Dispose(false);
+            }
+
+            public void SomeMethod()
+            {
+                if (_isDisposed)
+                {
+                    throw new ObjectDisposedException("ResourceHolder"); 
+                }
+            }
+        }
+        
+    }
+}
