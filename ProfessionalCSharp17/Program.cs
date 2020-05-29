c73c10f766db9d57d6dbd1015afc06bc49432900
ChenJie <530216775@qq.com>
2020-05-29 23:49:30 +0800

    只读引用

--- ProfessionalCSharp17/Program.cs
+++ ProfessionalCSharp17/Program.cs
@@ -9,6 +9,42 @@ namespace ProfessionalCSharp17
             Console.WriteLine("Hello World!");
         }
 
+        public class Data
+        {
+            public Data(int anumber) => _anumber = anumber;
+
+            private int _anumber;
+
+            public ref int GetNumber() => ref _anumber;
+
+            public ref readonly int GetReadonlyNumber() => ref _anumber;
+
+            public void Show() => Console.WriteLine($"Data:{_anumber}");
+        }
+
+        static void UseMember()
+        {
+            Console.WriteLine(nameof(UseMember));
+            var d = new Data(11);
+            int n = d.GetNumber();
+            n = 42;
+            d.Show();
+            Console.WriteLine();
+        }
+
+        static void UseRefMember()
+        {
+            Console.WriteLine(nameof(UseRefMember));
+
+            var d = new Data(11);
+
+            ref int n = ref d.GetNumber();
+            n = 42;
+            d.Show();
+            Console.WriteLine();
+        }
+
+
         public class ResourceHolder : IDisposable
         {
             private bool _isDisposed = false;
